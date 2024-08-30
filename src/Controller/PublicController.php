520d41a164bdf7d96f1294c42fd28f6037239e32
sebastien2506 <jestrovic_8@msn.com>
2024-08-30 09:10:05 +0200

    sebsym

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PublicController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('public/index.html.twig', [
+        
+        ]);
+    }
+    #[Route('/page/{id}',
+    name: 'page',
+    requirements:['id'=>'\d+'],
+    defaults:['id'=>1],
+    methods:['GET'] 
+    )]
+    public function page($id): Response
+    {
+        
+        return $this->render('public/page.html.twig', [
+            'page' => $id,
+        ]);
+}
+    #[Route('/page/haha',
+    name: 'error',
+    methods:['GET'] 
+    )]
+    public function error(): Response
+    {
+        
+        return $this->render('public/error.html.twig', [
+    
+        ]);
+}
+
+}
